21d3bc10a443c250ab8a245668c90b70016687bd
Globx <87949375+Globx@users.noreply.github.com>
2022-02-01 16:28:53 -0800

    Initial commit
    Merged OceanDream into oled keymap with VIA support.

---
+++ keyboards/nullbitsco/nibble/keymaps/oceandream/keymap.c
@@ -0,0 +1,130 @@
+/* Copyright 2021 Jay Greco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "ocean_dream.h"
+
+enum layer_names {
+  _BASE,
+  _VIA1,
+  _VIA2,
+  _VIA3
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_BASE] = LAYOUT_all(
+              KC_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
+   KC_MUTE,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
+    RGB_TOG,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    KC_F15,   KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    KC_F16,   KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(_VIA1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+
+  [_VIA1] = LAYOUT_all(
+               KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_INSERT,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_SLCK,
+    _______,  _______, _______, _______, _______, _______, _______,   RESET, _______, _______, _______, _______, _______, _______, _______, KC_PAUSE,
+    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
+  ),
+
+  [_VIA2] = LAYOUT_all(
+              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
+  ),
+
+  [_VIA3] = LAYOUT_all(
+              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+    _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______,  _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
+  ),
+
+};
+
+#ifdef OLED_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) { 
+    return OLED_ROTATION_90; 
+}
+
+
+bool oled_task_user(void) {
+    #    ifdef OCEAN_DREAM_ENABLE
+        render_stars();
+    #    endif
+    return false;
+}
+#endif
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // Send keystrokes to host keyboard, if connected (see readme)
+    process_record_remote_kb(keycode, record);
+    switch(keycode) {
+        case KC_LCTL:
+        case KC_RCTL:
+        #ifdef OCEAN_DREAM_ENABLE
+          is_calm = (record->event.pressed) ? true : false;
+        #endif
+        break;        
+
+        case RM_1: //remote macro 1
+        if (record->event.pressed) {
+        }
+        break;
+
+        case RM_2: //remote macro 2
+        if (record->event.pressed) {
+        }
+        break;
+
+        case RM_3: //remote macro 3
+        if (record->event.pressed) {
+        }
+        break;
+
+        case RM_4: //remote macro 4
+        if (record->event.pressed) {
+        }
+        break;
+
+    }
+    return true;
+}
+
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return true;
+}
+
+void matrix_init_user(void) {
+    // Initialize remote keyboard, if connected (see readme)
+    matrix_init_remote_kb();
+}
+
+void matrix_scan_user(void) {
+    // Scan and parse keystrokes from remote keyboard, if connected (see readme)
+    matrix_scan_remote_kb();
+}
